c3e32eb55cc0c6108651741a35f57364d2af530c
Nicholas Wolf <nicholas.wolf@nyu.edu>
2018-08-01 16:48:34 -0400

    Small updates to Pedestrian class

--- pedestrian.py
+++ pedestrian.py
@@ -1,12 +1,13 @@
-from traffic_simuator import GeoLocation
+from traffic_simuator import CityLocation
 
 class Pedestrian(object):
 
-    def __init__(self, start_location: GeoLocation = None, current_location: GeoLocation = None, destination: GeoLocation = None):
+    def __init__(self, name: str, start_location: CityLocation, destination: CityLocation, list_short_paths):
 
         self.start_location = start_location
-        self.current_location = current_location
+        self.list_short_paths = list_short_paths
         self.destination = destination
+        self.name = name
 
     def __str__(self) -> str:
-        return "Started: {}, Currently: {}, Destination: {})".format(self.start_location, self.current_location, self.destination)
+        return "Name: {}, Started: {}, Destination: {})".format(self.name, self.start_location, self.destination)
